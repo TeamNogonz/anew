f6849604a90540d3345dc1b6dd7f26596a390b36
jinseok1018 <pjpj1018@naver.com>
2025-06-28 17:01:38 +0900

    [Fix] Conflict solve

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+<<<<<<< HEAD
 import { RouterProvider } from 'react-router-dom';
 import router from './routes';
 import './App.css';
 
 function App() {
   return <RouterProvider router={router} />;
+=======
+import React from 'react';
+
+function App() {
+  return (
+    <iframe
+      src={process.env.PUBLIC_URL + '/home.html'}
+      title="Anew Home"
+      style={{ width: '100vw', height: '100vh', border: 'none' }}
+    />
+  );
+>>>>>>> 6833d8a ([Add] home page init)
 }
 
 export default App;
